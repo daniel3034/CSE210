4815f15b36829aa357de0f185b30560f3348c71f
daniel3034 <danielh3034@gmail.com>
2024-04-27 23:47:00 -0500

    guess a number ok

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,14 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        
         int guess = -1;
-        int number = 0;
 
         while (guess != number)
         {
-            Console.Write("What is the magic number? ");
-            number = int.Parse(Console.ReadLine());
-
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
