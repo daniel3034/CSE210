9d82e3ae2a95a15d54311628002d1d86531d4bb9
daniel3034 <danielh3034@gmail.com>
2024-04-27 23:41:32 -0500

    not working

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        int guess = -1;
+        int number = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is the magic number? ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number == guess)
+            {
+                Console.WriteLine("You guess it!");
+            }
+            else
+            {
+                Console.WriteLine("Lower");  
+            }
+        }
+    }    
 }
