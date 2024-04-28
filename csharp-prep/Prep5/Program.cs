c4debd9e1cc89acc746106b9010f904a13af573b
daniel3034 <danielh3034@gmail.com>
2024-04-28 00:49:38 -0500

    prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        welcome();
+
+        string username = promptusername();
+
+        int number = promptusernumber();
+
+        int square = Squarenumber(number);
+
+        Displayresult(username, square);
+    }
+
+    static void welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string promptusername()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int promptusernumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int Squarenumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Displayresult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
